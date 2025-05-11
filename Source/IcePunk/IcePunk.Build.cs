289d26ebd88c7f0b95e1e51e1f4a1152d628aac6
Kazamapilot <kazamapilot@gmail.com>
2025-05-11 18:29:07 +0530

    lo

---
+++ Source/IcePunk/IcePunk.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class IcePunk : ModuleRules
+{
+	public IcePunk(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/IcePunk/IcePunkGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IcePunkGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AIcePunkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIcePunkGameMode();
+};
+
+
+
